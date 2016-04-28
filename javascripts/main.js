b8a9ecb5c01ac497843e7929726b1a97cfa2841d
Ruben Piatnitsky <r.piatnitsky@gmail.com>
2016-04-28 13:55:38 -0700

    Made text updates and fxed the contact jumbotron - Added the contact info to the Projects page. - Dropped the project javascript into the main.js - Used toggle instead of slide up and down for jquery function of contact jumbotron. - Updated the description below the image and name on index page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+//console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+  var menuContact = $("#menuContact");
+
+  menuContact.click(function(){
+    var contactPanel = $("#contactPanel");
+    contactPanel.slideToggle();
+  });
+
+  // Project page side menu
+  $(".sidemenu").click(function(){
+      var itemId = $(this).data("item");
+
+      // Set the item to be active after removing other active items.
+      $(".sidemenu.active").removeClass("active").removeClass("left-border-emph");
+      $(this).addClass("active").addClass("left-border-emph");
+
+      // Hide the previous pane
+      $(".pane:visible").each(function(){
+          $(this).hide()
+      });
+
+      // Show the selected Item pane
+      $("#"+itemId).show();
+  });
+});
