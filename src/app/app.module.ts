add93ca03345f3a7c920efe05876b391c7187091
Ruben <rub3@pdx.edu>
2018-03-04 18:51:41 -0800

    Add the education component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatChipsModule, MatChip, MatChipList} from '@angular/material';
 
 import {AppComponent} from './app.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
