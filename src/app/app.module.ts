eb3e10c8bfbc2981bf812c078029610a0ecb2006
Ruben <rub3@pdx.edu>
2018-03-04 19:07:01 -0800

    Add the about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatChipsModule, MatChip, MatChipList} from '@angular/material';
+import {MatChipsModule, MatCardModule, MatMenuModule, MatToolbarModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import { EducationComponent } from './education/education.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EducationComponent,
     ExperienceComponent,
-    SkillsComponent
+    SkillsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatChipsModule,
-    MatChip,
-    MatChipList
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
