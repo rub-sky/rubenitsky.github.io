5b98765c9d15f473040a54364f67824866354e71
Ruben <rub3@pdx.edu>
2018-03-13 21:48:24 -0700

    Change project to use bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatChipsModule, MatCardModule, MatMenuModule, MatToolbarModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import { EducationComponent } from './education/education.component';
@@ -19,11 +18,7 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatChipsModule,
-    MatCardModule,
-    MatMenuModule,
-    MatToolbarModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
